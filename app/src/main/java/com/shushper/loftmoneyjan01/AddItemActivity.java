de311022cb87f017b37578588a616554f8152cad
Renat Shakhmatov <shushper@gmail.com>
2019-02-01 23:28:51 +0500

    AddItemActivity

---
+++ app/src/main/java/com/shushper/loftmoneyjan01/AddItemActivity.java
@@ -0,0 +1,47 @@
+package com.shushper.loftmoneyjan01;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddItemActivity extends AppCompatActivity {
+
+    private EditText name;
+    private EditText price;
+    private Button addBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+
+        name = findViewById(R.id.name);
+        price = findViewById(R.id.price);
+        addBtn = findViewById(R.id.add_btn);
+
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                addBtn.setEnabled(!TextUtils.isEmpty(s));
+            }
+        });
+
+
+    }
+}
