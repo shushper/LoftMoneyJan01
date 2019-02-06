a76a9ebb1d1fa74fcff3cbf1ac5e172540be420b
Renat Shakhmatov <shushper@gmail.com>
2019-02-06 23:51:44 +0500

    ItemsFragment

--- app/src/main/java/com/shushper/loftmoneyjan01/MainActivity.java
+++ app/src/main/java/com/shushper/loftmoneyjan01/MainActivity.java
@@ -1,14 +1,39 @@
 package com.shushper.loftmoneyjan01;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager pager;
+    private TabLayout tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "onCreate");
+
+        MainPagesAdapter adapter = new MainPagesAdapter(getSupportFragmentManager(), this);
+
+        pager = findViewById(R.id.pager);
+        tabs = findViewById(R.id.tabs);
+
+        tabs.setupWithViewPager(pager);
+
+        pager.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
     }
 }
